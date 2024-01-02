edc75b55ed5954dfd03366da6ab6a49f7dfd003d
MercyAkpan <akpanmeme@gmail.com>
2024-01-03 00:35:15 +0100

    alx python

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for ASCII_ALPHA in range (97, 123):
+for ASCII_ALPHA in range(97, 123):
     if ASCII_ALPHA in [101, 113]:
-	    continue;
+        continue
     print("{}".format(chr(ASCII_ALPHA)), end='')
