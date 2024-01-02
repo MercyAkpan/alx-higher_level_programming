b5f0dda770bdaa6d6209be59a32d326275fb8fae
MercyAkpan <akpanmeme@gmail.com>
2024-01-02 23:26:24 +0100

    alx python

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,8 +6,8 @@ if number < 0:
 	ldigit = -(ldigit)
 motif = "Last digit of {} is ".format(number)
 if (ldigit > 5):
-	print(f"{motif}{ldigit} and is greater than 5")
+    print(f"{motif}{ldigit} and is greater than 5")
 elif ldigit == 0:
-	print(f"{motif}{ldigit} and is 0")
+    print(f"{motif}{ldigit} and is 0")
 elif ldigit < 6 and ldigit != 0:
-	print(f"{motif}{ldigit} and is less than 6 and not 0")
+    print(f"{motif}{ldigit} and is less than 6 and not 0")
