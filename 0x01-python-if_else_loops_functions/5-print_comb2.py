09648c6f3316ea2e99166a158939dd1e505469bd
MercyAkpan <akpanmeme@gmail.com>
2024-01-03 01:17:18 +0100

    alx python

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,1 +1,8 @@
-#
+#!/usr/bin/python3
+for numbers in range(0, 100):
+    if numbers in [99]:
+        print("{:02d}".format(numbers), end='\n')
+# one could put precision with the string itself with ""
+# not with '' and not within .format()
+        continue
+    print("{:02d}".format(numbers), end=', ')
