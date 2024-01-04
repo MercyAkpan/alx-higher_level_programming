c6c426e6c742e9bdc79f82414a50d5a5e171ac9b
MercyAkpan <akpanmeme@gmail.com>
2024-01-04 21:43:06 +0100

    alx python

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ ==  "__main__":
+    from sys import argv
+    count = len(argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    if count == 1:
+        print("1 argument:")
+    if count > 1:
+        print(f"{count} arguments:")
+    if count >= 1:
+        for iter in range(1, count + 1):
+            print("{}: {}".format(iter, argv[iter]))
