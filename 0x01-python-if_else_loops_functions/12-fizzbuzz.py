787a67f62f85e09d6359bc6fcffb6e78c032bd2e
MercyAkpan <akpanmeme@gmail.com>
2024-01-05 06:33:04 +0100

    python

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,1 +1,11 @@
-#
+#!/usr/bin/python3
+def fizzbuzz():
+    for iter in range(0, 101):
+        if iter % 3 == 0:
+            print("Fizz", end=" ")
+        elif iter % 5 == 0:
+            print("Buzz", end=" ")
+        elif iter % 5 and 3 == 0:
+            print("FizzBuzz", end=" ")
+        else:
+            print("{}".format(iter), end=" ")
