4c0ceeb58a74af3ad242d2de8971c69656da557e
MercyAkpan <akpanmeme@gmail.com>
2024-01-05 06:42:23 +0100

    python

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for iter in range(1, 101):
-        if iter % 3 == 0:
+#if iter % 5 and iter % 3 == 0:
+#print("FizzBuzz", end=" ")
+#why didn't the ABOVE work??
+        if iter % 15 == 0:
+            print("FizzBuzz", end=" ")
+        elif iter % 3 == 0:
             print("Fizz", end=" ")
         elif iter % 5 == 0:
             print("Buzz", end=" ")
-        elif iter % 5 and 3 == 0:
-            print("FizzBuzz", end=" ")
         else:
             print("{}".format(iter), end=" ")
