abb5460a969b0c57011504f3b5a4d24b1ce578cb
MercyAkpan <akpanmeme@gmail.com>
2024-01-05 05:47:04 +0100

    python

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     from hidden_4 import *
     allfiles = dir()
     for iter in range(len(allfiles)):
-        if (allfiles[iter][-2:]) == "__":
+        if (allfiles[iter][:2]) == "__":
             continue
         print("{}".format(allfiles[iter]))
