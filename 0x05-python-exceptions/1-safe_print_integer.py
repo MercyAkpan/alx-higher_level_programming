83f0bcaada93bef486ab688465105b50e7aeaaec
MercyAkpan <akpanmeme@gmail.com>
2024-01-24 01:31:21 +0100

    alx

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,8 +2,8 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-        return (1)
+        return (True)
 # that is  if one tries to print it as an integer and no err
 # or occurs then, it returns 1 - True.
     except (TypeError, ValueError):
-        return (0)
+        return (False)
