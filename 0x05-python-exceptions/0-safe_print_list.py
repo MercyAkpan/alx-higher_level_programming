0e1305b2fcaa072a70c72db6eef849c7be6327d3
MercyAkpan <akpanmeme@gmail.com>
2024-01-24 01:04:32 +0100

    alx

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     actualen = 0
+# couunt for indexe in string.
     for iter in range(x):
         try:
             print("{}".format(my_list[iter]), end="")
             actualen += 1
+# for loop to print all elemenst in string
         except Exception:
             break
+# except all Exception, for any exception encountered
     print("")
     return (actualen)
