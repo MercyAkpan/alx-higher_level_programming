447dfd475e84200f9484343a6f80108fab510566
MercyAkpan <akpanmeme@gmail.com>
2024-01-25 03:06:59 +0100

    alx

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Provide1  class Square."""
+
+
+class Square:
+    """Display a square."""
+
+    def __init__(self, size):
+        """Init new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
