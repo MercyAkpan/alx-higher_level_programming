30ee6f079eb3eb2672aa4385d1e95f21e5ee5082
MercyAkpan <akpanmeme@gmail.com>
2024-01-25 03:59:11 +0100

    alx

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,17 +1,4 @@
 #!/usr/bin/python3
-
-def safe_print_integer(value):
-    """Print an integer with "{:d}".format().
-
-    Args:
-        value (int): The integer to print.
-
-    Returns:
-        If a TypeError or ValueError occurs - False.
-        Otherwise - True.
-    """
-    try:
-        print("{:d}".format(value))
-        return (True)
-    except (TypeError, ValueError):
-        return (False)
+def search_replace(my_list, search, replace):
+    new_list = list(map(lambda y: replace if y == search else y, my_list))
+    return (new_list)
