9266517af0e68eeb1a0d57fa77f140636fff6eef
MercyAkpan <akpanmeme@gmail.com>
2024-01-31 16:42:55 +0100

    alx

--- 0x09-python-everything_is_object/trial.py
+++ 0x09-python-everything_is_object/trial.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def increment(n):
     n += 1
-
+    return(n) # I added a return fn to be the object 'b' points to.
 a = 2
-increment(a)
-print(a)
+# I use the b to have a new object to point to.
+b = increment(a)
+print(b)
