4ca316eb7d67fef6a7a27e94cf6e5d7f98264478
MercyAkpan <akpanmeme@gmail.com>
2024-02-25 22:31:57 +0100

    alx

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,21 +1,42 @@
 #!/usr/bin/python3
-"""Module 7 -add_item
-Adds all arguments to a python list and then saves to file
-"""
 
+"""
+A script that adds all arguments to a Python list, and then save them to a
+JSON file.
+"""
 
 import sys
 import json
-import os.path
 
-if __main__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+
+def update_json_file(args, json_file: str = "add_item.json") -> None:
+    """
+    Adds command line all arguments to a Python list, and then save them to
+    a JSON file.
+
+    Args:
+        args (list): A list command line arguments.
+        json_file (str): The name of JSON file to write to. Defaults to
+        "add_item.json"
+    """
+    py_list = []
+
+    # create the Python list from the JSON file, and handle errors as needed
     try:
-        content = load_from_json_file("add_item.json")
-    except FilenotFoundError:
-        content = []
-    content.extend(sys.arg[1:])
-    save_to_json_file(content, "add_items.json")
+        py_list = load_from_json_file(json_file)
+    except Exception:
+        # let's create the file, it doesn't exist
+        save_to_json_file([], json_file)
+
+    # add all the arguments to the lists
+    for arg in args:
+        py_list.append(arg)
+
+    save_to_json_file(py_list, json_file)
+
+
+if __name__ == "__main__":
+    update_json_file(sys.argv[1:])
