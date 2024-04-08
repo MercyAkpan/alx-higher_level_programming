fe5249a1ff37e35dd786cec79574dfdf5254b1e8
MercyAkpan <akpanmeme@gmail.com>
2024-04-09 00:49:43 +0100

    alx

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+function ToInteger (arg) {
+  // Use Number() to attempt conversion with explicit checking for NaN
+  const convert = Number(arg); /* Better than parseint() that only converts portion of string
+                                to an integer, stopping at 1st non-numeric xter */
+  let i = 0;
+  if (Number.isNaN(convert)) {
+    console.log('Missing number of occurrences');
+  } else {
+    while (i < convert) {
+      console.log('C is fun');
+      i++;
+    }
+  }
+}
+// Example usage:
+const argv = process.argv;
+ToInteger(argv[2]);
