1da99650b7dde7d081c9037f95cfa95fcf7119e3
MercyAkpan <akpanmeme@gmail.com>
2024-04-08 23:51:06 +0100

    alx

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 // Ctrl- G to get name of file
+// Ctrl -W x2 - to switch files
 const argv = process.argv; // This gets the process.argv property(command- line arguments)
 if (!argv[2]) { // checks if 3rd args, exists or is undefined
   console.log('No argument');
---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+
+const argv = process.argv;
+const verb = ' is ';
+console.log(argv[2] + verb + argv[3]);
