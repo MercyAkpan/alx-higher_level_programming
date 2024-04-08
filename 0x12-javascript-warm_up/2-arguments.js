07e1e77349f154e3e85aecf2b1b5f38355069e73
MercyAkpan <akpanmeme@gmail.com>
2024-04-08 22:54:04 +0100

    alx

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 
-const argv = process.argv.length
-	if (argv == 2){
-		console.log("No argument")
-	}
-	else if (argv == 3){
-		console.log("Argument found")
-	}
-	else if (argv > 3){
-		console.log("Arguments found")
-	}
+const argv = process.argv.length;
+if (argv === 2) {
+  console.log('No argument');
+} else if (argv === 3) {
+  console.log('Argument found');
+} else if (argv > 3) {
+  console.log('Arguments found');
+}
