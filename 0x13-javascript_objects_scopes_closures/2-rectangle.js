c1c176435b17ccc48dcae16b913f75b92be131fd
MercyAkpan <akpanmeme@gmail.com>
2024-04-09 22:53:58 +0100

    alx

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-
+// MINE
 class Rectangle {
   constructor (w, h) {
     if (!(w <= 0 || h <= 0 || !w || !h)) { // This ensures both arguments are undefined if w or h don't exist; or either is <= 0
-      this.width = w;
-      this.height = h;
+      this.width = w; // This doesn't create an empty object, but if the condition is not met, it doesnt init any value for
+      this.height = h; // w, h passed. They come out as undefined, and the class prints out no argument.
     }
   }
 }
