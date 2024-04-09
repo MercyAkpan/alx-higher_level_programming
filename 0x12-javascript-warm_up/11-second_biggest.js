63c9f804635ad951383355f21339849d3907456a
MercyAkpan <akpanmeme@gmail.com>
2024-04-09 03:25:01 +0100

    alx

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@
 function SecBiggest (a) {
   const len = a.length - 2;
   if (len === 0 || len === 1) { console.log(0); }
-  console.log(a.slice(2).sort()[1]);
+  console.log(a.slice(2).sort()[len - 2]);
 }
 const argv = process.argv;
 SecBiggest(argv);
