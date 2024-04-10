cfcc1f2cbbebd12dce948d1d1878d85431b9b1da
MercyAkpan <akpanmeme@gmail.com>
2024-04-11 00:11:46 +0100

    alx

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -9,12 +9,12 @@ function SecBiggest (a) {
     return;
   }
   while (i < len) {
-    b[i] = Number(b[i]);
+    b[i] = Number(b[i]); // converts all elements to numbers
     i++;
   }
-  const sortedNumbers = b.sort((a, j) => a - j);
-  console.log(sortedNumbers[sortedNumbers.length - 2]);
-//  console.log((b.sort()[len - 2]));
+  const sortedNumbers = b.sort((a, j) => a - j); // an inbuilt function to sort (modifying) the b array
+  console.log(sortedNumbers[sortedNumbers.length - 2]); // obtain 2nd to last index
+//  console.log((b.sort()[len - 2])); // This sorts based on unicode: [1<10<11<3<5 not [1<3<5<10<11]
 }
 const argv = process.argv;
 SecBiggest(argv);
