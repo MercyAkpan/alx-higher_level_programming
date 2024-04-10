7f5c362042f79ce98d0155af5c1d2a6a555ea31b
MercyAkpan <akpanmeme@gmail.com>
2024-04-10 20:56:42 +0100

    alx

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -11,24 +11,6 @@ class Square extends vars { // here Square extends the class in const vars (=== 
     } else {
 		this.a = c // This access the superclass variable and define it as c. Not this.print().a = c DIDN'T WORK
 		this.print() // This then acees the print variable with a = c.
-//      let i = 0;
-//      let j = 0;
-//      try { // To catch RangeError for -ve numbers
-//        const array = new Array(this.height); // create a new array
-//        while (i < this.height) {
-//          array[i] = new Array(this.width); // create a new array for every element of the array - 2D
-//          while (j < this.width) {
-//            array[i].push(this.c); // array property to push in an element
-//            j++;
-//          } // j = convert
-//          j = 0; // Reassign j to 0.
-//          i++;
-//        }
-//        for (i = 0; i < this.height; i++) { // print every element in a sub array , together as a string separated by ""
-//          console.log(array[i].join(''));
-//        }
-//      } catch (RangeError) {
-//      }
     }
   }
 }
