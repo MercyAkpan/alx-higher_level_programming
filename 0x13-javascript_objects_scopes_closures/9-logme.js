cb3fa09e0e74b507dbb914c9018f854d5c588006
MercyAkpan <akpanmeme@gmail.com>
2024-04-10 22:05:42 +0100

    alx

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-let num = 0;
-exports.logMe = function (item) {
-  console.log(num + ':' + item);
-  num += 1;
+// MINE.
+let num = 0; // Here num has a module scope.
+exports.logMe = function (item) { // logMe creates a closure , by maintaining ref. to the num variable
+  console.log(num + ': ' + item); // even after returning.
+  num += 1; // this value of num is 'remembered' even after returning the function
 };
