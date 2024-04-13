e94511efe01644bae013b3ac48e60db6c992b627
MercyAkpan <akpanmeme@gmail.com>
2024-04-13 09:47:58 +0100

    alx

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import MySQLdb
+"""MySQLdb is  a python interface for connecting to databases. """
 db = MySQLdb.connect(host='localhost', user='meme', password='password1',
                      port=3306, db='hbtn_0e_0_usa')
 cur = db.cursor()
