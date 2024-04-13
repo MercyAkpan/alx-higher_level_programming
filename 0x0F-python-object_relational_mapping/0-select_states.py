6d8de564ad0032f9162ea2552a4642dd39af4a0b
MercyAkpan <akpanmeme@gmail.com>
2024-04-13 09:57:35 +0100

    alx

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-import MySQLdb
-"""MySQLdb is  a python interface for connecting to databases. """
-db = MySQLdb.connect(host='localhost', user='meme', password='password1',
-                     port=3306, db='hbtn_0e_0_usa')
-cur = db.cursor()
+if __name__ = '__main__':
+    import MySQLdb
+    """MySQLdb is  a python interface for connecting to databases. """
+    db = MySQLdb.connect(host='localhost', user='meme', password='password1',
+                         port=3306, db='hbtn_0e_0_usa')
+    cur = db.cursor()
 
-cur.execute("select * from states")
-result = cur.fetchall()  # to fetch result from cur.execute
-for data in result:
-    print(data)
+    cur.execute("select * from states")
+    result = cur.fetchall()  # to fetch result from cur.execute
+    for data in result:
+        print(data)
