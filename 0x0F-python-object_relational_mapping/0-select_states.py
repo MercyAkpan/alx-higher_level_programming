e16c8d046d1c398c6738c142cb05adba4d94b9bc
MercyAkpan <akpanmeme@gmail.com>
2024-04-13 11:08:42 +0100

    alx

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
+"""MySQLdb is  a python interface for connecting to databases. """
+import MySQLdb
+import sys
 if __name__ == '__main__':
-    import MySQLdb
-    """MySQLdb is  a python interface for connecting to databases. """
-    db = MySQLdb.connect(host='localhost', user='root', password='root',
-                         port=3306, db='hbtn_0e_0_usa')
+    user_name = sys.argv[1]
+    user_password = sys.argv[2]
+    database = sys.argv[3]
+    db = MySQLdb.connect(host='localhost', user=user_name,
+                         password=user_password,
+                         port=3306, db=database)
     cur = db.cursor()
 
-    cur.execute("select * from states")
+    cur.execute("SELECT * FROM states")
     result = cur.fetchall()  # to fetch result from cur.execute
     for data in result:
         print(data)
+    cur.close()
     db.close()
