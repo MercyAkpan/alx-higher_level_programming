f4102dc632d5df1ebc8415ef020756708f6ef833
MercyAkpan <akpanmeme@gmail.com>
2024-04-15 23:15:31 +0100

    alx

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -7,22 +7,22 @@ from sqlalchemy.ext.declarative import declarative_base
 import sys
 
 Base = declarative_base()
-user = sys.argv[1]
-pswd = sys.argv[2]
-db = sys.argv[3]
-connection = f"mysql+mysqldb://{user}:{pswd}@localhost:3306/{db}"
-engine = create_engine(connection)
-Base.metadata.create_all(bind=engine)
-Session = sessionmaker(bind=engine)
-session = Session()
+# user = sys.argv[1]
+# pswd = sys.argv[2]
+# db = sys.argv[3]
+# connection = f"mysql+mysqldb://{user}:{pswd}@localhost:3306/{db}"
+# engine = create_engine(connection)
+# Base.metadata.create_all(bind=engine)
+# Session = sessionmaker(bind=engine)
+# session = Session()
 
 
 class State(Base):
     """ THIS IS A CLASS MAPPED TO A TABLE
     """
     __tablename__ = "states"
-    id = Column(Integer(), primary_key=True)
-    name = Column(String(128), nullable=False)
+    id = Column(Integer(), primary_key=True, autoincrement=True)
+    name = Column(String(128))
 
 #    def __init__(self, name):
 #        """ THIS IS THE INIT FUNCTION
