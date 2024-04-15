1b14a7c7e202ca408b14a4477c17de7203c892bb
MercyAkpan <akpanmeme@gmail.com>
2024-04-15 21:09:26 +0100

    alx

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" SQLALCHEMY MODULE """
+from sqlalchemy import create_engine, Column, String, Integer
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """ THIS IS A CLASS MAPPED TO A TABLE
+    """
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+
+    def __init__(self, name):
+        """ THIS IS THE INIT FUNCTION
+        """
+        self.name = name
+
+
+engine = create_engine("""mysql+mysqldb://meme
+                       :password1@localhost/
+                       hbtn_0e_6_usa""", echo=True)
+Base.metadata.create_all(bind=engine)
+Session = sessionmaker(bind=engine)
+session = Session()
