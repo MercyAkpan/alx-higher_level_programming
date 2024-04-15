f916004c82c7375c43092ae74c80d41d727beaf7
MercyAkpan <akpanmeme@gmail.com>
2024-04-16 00:14:45 +0100

    alx

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" SQLALCHEMY MODULE """
+from sqlalchemy import create_engine, Column, String, Integer
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base
+from model_state import Base, State
+import sys
+if __name__ == "__main__":
+    Base = declarative_base()
+    user = sys.argv[1]
+    pswd = sys.argv[2]
+    db = sys.argv[3]
+    connection = f"mysql+mysqldb://{user}:password1@localhost:3306/{db}"
+    engine = create_engine(connection)
+    Base.metadata.create_all(bind=engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    res = session.get(State, 1)
+    print(f"{res.id}: {res.name}")
+# ABOVE. This  returns a list of the State objects
+#     for data in res:
+#         print(f"{data.id}: {data.name}")
+#     # To access the list(res), use each row attribute, not index
+    session.close()
