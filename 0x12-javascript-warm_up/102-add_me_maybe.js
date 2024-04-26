d631a7f649e925159d6014f134be15308d641bf6
MercyAkpan <akpanmeme@gmail.com>
2024-04-26 19:17:59 +0100

    alx

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,3 +1,4 @@
+#!/usr/bin/node
 exports.addMeMaybe = function (number, theFunction) {
   theFunction(++number); // Pre-increment the number before sending through to function, not post-increment
 };
