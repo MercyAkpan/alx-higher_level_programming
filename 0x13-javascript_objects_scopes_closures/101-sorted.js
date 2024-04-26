1f44bd80e8398fea640864272d8f1aafdd7be266
MercyAkpan <akpanmeme@gmail.com>
2024-04-26 23:43:46 +0100

    alx:

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,8 +1,14 @@
 #!/usr/bin/node
+// This takes a dictionary , and fromats it into a new dictionary , based on number of occurences 
+// of each key.
+//const fs = require('fs');
+//const data = fs.readFileSync('./101-data.js', 'utf8');
+//const jsonData = JSON.parse(data.replace(/^(module\.exports = )?(.*)$/, '')); // Capture the value (optional leading whitespace)
+//const { dict } = jsonData; // Access the 'dict' property
 const dict1 = require('./101-data.js').dict;
 console.log(dict1);
 const dict2 = {};
-for (const [key, value] of Object.entries(dict1)) {
+for (const [key, value] of Object.entries(dict1)) { // Object.entries is used to destructure dictionaries well in JS.
   if (dict2[value]) {
     dict2[value].push(key);
   } else {
