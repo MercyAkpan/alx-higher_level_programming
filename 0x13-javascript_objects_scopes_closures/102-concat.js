99ce9fb74e5a3627a186f70dbb571787fde934af
MercyAkpan <akpanmeme@gmail.com>
2024-04-27 14:35:47 +0100

    alx

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,8 +1,12 @@
 #!/usr/bin/node
 const args = process.argv;
-const fs = require('fs');
-const file1 = fs.readFileSync(args[2], 'utf8'); // To read a file from command line argument.
-const file2 = fs.readFileSync(args[3], 'utf8');
-const file3 = file1 + file2;
-fs.writeFileSync('file3', file3, 'utf8');
-console.log(file3);
+try {
+  const fs = require('fs');
+  const file1 = fs.readFileSync(args[2], 'utf8'); // To read a file from command line argument.
+  const file2 = fs.readFileSync(args[3], 'utf8');
+  const file3 = file1 + file2;
+  fs.writeFileSync('file3', file3, 'utf8');
+  console.log(file3);
+} catch (error) {
+  console.error('Nope');
+}
