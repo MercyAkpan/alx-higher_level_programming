8fa96e0704c52828382b1282b9821c36d8590643
MercyAkpan <akpanmeme@gmail.com>
2024-04-27 14:44:03 +0100

    alx

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -5,8 +5,7 @@ try {
   const file1 = fs.readFileSync(args[2], 'utf8'); // To read a file from command line argument.
   const file2 = fs.readFileSync(args[3], 'utf8');
   const file3 = file1 + file2;
-  fs.writeFileSync('file3', file3, 'utf8');
-  console.log(file3);
+  fs.writeFileSync(args[4], file3, 'utf8');
 } catch (error) {
   console.error('Nope');
 }
