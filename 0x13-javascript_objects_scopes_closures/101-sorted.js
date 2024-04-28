7f96ffc0a8fbf84a9da69baa9bb6b7177ac83621
MercyAkpan <akpanmeme@gmail.com>
2024-04-28 11:15:28 +0100

    alx

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,25 +1,21 @@
 #!/usr/bin/node
-// This takes a dictionary , and fromats it into a new dictionary , based on number of occurences
-// of each key.
-// const fs = require('fs');
-// const data = fs.readFileSync('./101-data.js', 'utf8');
-// const jsonData = JSON.parse(data.replace(/^(module\.exports = )?(.*)$/, '')); // Capture the value (optional leading whitespace)
-// const { dict } = jsonData; // Access the 'dict' property
-const dict1 = require('./101-data.js').dict;
-// console.log(dict1);
-const dict2 = {};
-for (const [key, value] of Object.entries(dict1)) { // Object.entries is used to destructure dictionaries well in JS.
-  if (dict2[value]) {
-    dict2[value].push(key);
-  } else {
-    dict2[value] = [key];
+
+/**
+ * This script imports a dictionary of occurrences by user id and computes a
+ * dictionary of user ids by occurrence.
+ *
+ * In order words, it computes a new dictionary with the values as keys and
+ * the keys as values.
+ */
+
+const dict = require('./101-data').dict;
+const newDict = {};
+
+for (const key in dict) {
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [];
   }
+  newDict[dict[key]].push(key);
 }
-// console.log(dict2);
-// const formattedDict = Object.entries(originalDict).map(([key, value]) => ({
-//  [key]: (key === "age" && value >= minAge) ? "Eligible" : value
-// }));
-// for (const [Count, IdList] of Object.entries(dict2)) {
-//  const UserIds = IdList.join(', '); // Join user IDs with commas
-//  console.log(`key: ${Count} - ${UserIds}`); // Formatted output
-// }
+
+console.log(newDict);
