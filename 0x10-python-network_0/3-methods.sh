9ada2b59153b8aa98b6873055bf0a9c271ed8b09
MercyAkpan <akpanmeme@gmail.com>
2024-05-06 05:00:42 +0100

    3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-allowed_methods=$(curl -sI -X OPTIONS -I "$1" | grep -i "^Allow:" | cut -d " " -f 2-)
-echo "$allowed_methods"
+# comment
+allowed_methods=$(curl -sI -X OPTIONS -I "$1" | grep -i "^Allow:" | cut -d " " -f 2-) && echo "$allowed_methods"
