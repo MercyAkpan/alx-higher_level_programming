a91bda1182c78588a0f238771c44852918a07b55
MercyAkpan <akpanmeme@gmail.com>
2024-05-06 04:53:46 +0100

    alx

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,9 @@
-#!/bin/bash
-# This script prints the allowed methods in a web request
-curl -sI "$1" | grep "Allow" | cut -d " " -f2-
+#!/usr/bin/env bash
+#This is to bring out accepted HTTP request by a server
+
+#  $1 => URL to test
+# Send an OPTIONS request to the server and capture the allowed methods
+allowed_methods=$(curl -sI -X OPTIONS -I "$1" | grep -i "^Allow:" | cut -d " " -f 2-)
+
+# Display the allowed methods
+echo "$allowed_methods"
