c949255a89628ebac7e5df6b57b1b4f0c3129356
MercyAkpan <akpanmeme@gmail.com>
2024-05-06 06:15:05 +0100

    alx

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # This add a temporary header to a HTTPS request and prints out body of response
 curl -sH "X-School-User-Id: 98" "$1"
-# var='X-meme-Id: 98'
-# NOTE: delimitter is a colon.
-# To view the header just added
-#curl -viH "$var" www.mercyakpan.tech 2>&1 | grep -i "$var" | awk -F': ' '{print substr($0, 3)}'
