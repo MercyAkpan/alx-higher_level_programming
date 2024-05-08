fd2f6acd01b3a2b405cfff4884873ab160bd932c
MercyAkpan <akpanmeme@gmail.com>
2024-05-08 22:55:47 +0100

    alx

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script sends a POST request (with data) to the passed URL, and displays the body of the response
-curl -sX POST "$1" -d "email=test@gmail.com" -d "subject=I will always be here for PLD"
+# This is a comment
+curl  POST -s -d "email=test@gmail.com" -d "subject=I will always be here for PLD"  "$1"  1>/dev/null
