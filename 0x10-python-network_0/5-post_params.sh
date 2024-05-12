dae1c5187e97434b32012c5f01b4a74fbd0db0bb
MercyAkpan <akpanmeme@gmail.com>
2024-05-12 14:30:38 +0100

    alx

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This is a comment
-curl  POST -s -d "email=test@gmail.com" -d "subject=I will always be here for PLD"  "$1"  1>/dev/null
+curl  POST -s -d "email=test@gmail.com" -d "subject=I will always be here for PLD"  "$1" # 1>/dev/null
