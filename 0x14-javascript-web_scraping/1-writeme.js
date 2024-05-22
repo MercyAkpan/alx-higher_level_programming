6b66b9b0ea325e80d0504bbdb73c920d841b67de
MercyAkpan <akpanmeme@gmail.com>
2024-05-22 21:17:19 +0100

    alx

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const writeToFile = (filePath, content) => {
+  try {
+    // Write the content to the file synchronously with UTF-8 encoding
+    fs.appendFileSync(filePath, content, 'utf-8');
+    console.log('File appended successfully.');
+  } catch (error) {
+    // Print the error object if an error occurred during writing
+    console.error('An error occurred:', error);
+  }
+};
+
+if (process.argv.length !== 4) {
+  console.log('Usage: node script.js <file_path> <content>');
+} else {
+  const filePath = process.argv[2];
+  const content = process.argv[3];
+  writeToFile(filePath, content);
+}
