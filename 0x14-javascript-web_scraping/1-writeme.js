f148d4872605ae121d26f7920f05d774a990fa21
MercyAkpan <akpanmeme@gmail.com>
2024-05-22 21:29:41 +0100

    alx

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,36 +1,15 @@
 #!/usr/bin/node
-//const fs = require('fs');
-//
-//const writeToFile = (filePath, content) => {
-//  try {
-//    // Write the content to the file synchronously with UTF-8 encoding
-//    fs.writeFileSync(filePath, content, 'utf-8');
-//    console.log('File written successfully.');
-//  } catch (error) {
-//    // Print the error object if an error occurred during writing
-//    console.error('An error occurred:', error);
-//  }
-//};
-//
-//if (process.argv.length !== 4) {
-//  console.log('Usage: node script.js <file_path> <content>');
-//} else {
-//  const filePath = process.argv[2];
-//  const content = process.argv[3];
-//  writeToFile(filePath, content);
-//}
 const fs = require('fs');
 
 const writeToFile = (filePath, content) => {
-  // Write the content to the file asynchronously with UTF-8 encoding
-  fs.writeFile(filePath, content, 'utf-8', (error) => {
-    if (error) {
-      // Print the error object if an error occurred during writing
-      console.error('An error occurred:', error);
-    } else {
-      console.log('File written successfully.');
-    }
-  });
+  try {
+    // Write the content to the file synchronously with UTF-8 encoding
+    fs.writeFileSync(filePath, content, 'utf-8');
+    console.log('File written successfully.');
+  } catch (error) {
+    // Print the error object if an error occurred during writing
+    console.error('An error occurred:', error);
+  }
 };
 
 if (process.argv.length !== 4) {
@@ -40,4 +19,3 @@ if (process.argv.length !== 4) {
   const content = process.argv[3];
   writeToFile(filePath, content);
 }
-
