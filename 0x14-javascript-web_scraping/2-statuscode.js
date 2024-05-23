aaf30640cc35ac9085a17515b06a707c2adcab02
MercyAkpan <akpanmeme@gmail.com>
2024-05-23 04:30:50 +0100

    alx

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,21 +1,36 @@
 #!/usr/bin/node
-const { exec } = require('child_process');
+//const { exec } = require('child_process');
+//
+//// URL to fetch
+//const url = process.argv[2] 
+////const url = "www.mercyakpan.tech" 
+//
+//// Execute curl command to get status code
+//exec(`curl -s -o /dev/null -I -w "%{http_code}" ${url}`, (error, stdout, stderr) => {
+//  if (error) {
+//    console.error(`Error: ${error.message}`);
+//    return;
+//  }
+//  if (stderr) {
+//    console.error(`Error: ${stderr}`);
+//    return;
+//  }
+//  // Print status code in the desired format
+//  console.log(`Code: ${stdout}`);
+//});
+//
+const request = require('request');
 
-// URL to fetch
-const url = process.argv[2] 
-//const url = "www.mercyakpan.tech" 
+// Check if the URL argument is provided
+const url = process.argv[2];
 
-// Execute curl command to get status code
-exec(`curl -s -o /dev/null -I -w "%{http_code}" ${url}`, (error, stdout, stderr) => {
+request(url, function (error, response, body) {
   if (error) {
-    console.error(`Error: ${error.message}`);
-    return;
+    // Print only the error object
+    console.error(error);
+  } else {
+    // Print the status code in the required format
+    console.log(`code: ${response.statusCode}`);
   }
-  if (stderr) {
-    console.error(`Error: ${stderr}`);
-    return;
-  }
-  // Print status code in the desired format
-  console.log(`Code: ${stdout}`);
 });
 
