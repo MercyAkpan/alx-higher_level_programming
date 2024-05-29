0e200db2268bb22e364ad7e18574cf9a003af257
MercyAkpan <akpanmeme@gmail.com>
2024-05-29 21:28:23 +0100

    alx

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,13 +2,13 @@
 const fs = require('fs');
 
 // Check if the correct number of arguments are provided
-  const filePath = process.argv[2];
-  const content = process.argv[3];
+const filePath = process.argv[2];
+const content = process.argv[3];
 
-  // Write the content to the file
-  fs.writeFile(filePath, content, 'utf-8', (error) => {
-    if (error) {
-      // Print the error object if an error occurred during writing
-      console.error(error);
-    }
-  });
+// Write the content to the file
+fs.writeFile(filePath, content, 'utf-8', (error) => {
+  if (error) {
+    // Print the error object if an error occurred during writing
+    console.error(error);
+  }
+});
