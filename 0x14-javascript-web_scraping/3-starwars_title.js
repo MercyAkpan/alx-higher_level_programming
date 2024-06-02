4bc71148b4f7e8f5f9fe7e03622e0d947a5d61c0
MercyAkpan <akpanmeme@gmail.com>
2024-06-02 23:20:00 +0100

    alx

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const request = require('request')
+const film_id = process.argv[2]
+//console.log(film_id)
+// Request URL
+let url = `https://swapi-api.alx-tools.com/api/films/${film_id}`
+console.log(url)
+request(url, { json: true }, (error, response, body) => {
+	// Printing the error if occurred
+	if (error) console.log(error)
+
+	// Printing status code
+//	console.log(response.statusCode);
+
+	// Printing body
+    trial = body
+    console.log(trial.title)
+});
+
