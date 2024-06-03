1e5cd0cdf46a46d6c4aa6cdd43e57edc42ebb5a3
MercyAkpan <akpanmeme@gmail.com>
2024-06-03 13:18:17 +0100

    alx

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,34 +1,22 @@
 #!/usr/bin/node
-const request = require('request');
-// This is the filmId of the movie. 
-const filmId = process.argv[2];
-// Request URL
-const url = `${filmId}`
-//console.log(url)
-// Getting the url, error, response, body
-// Without this json:true, the response wont be parse as JSON string
-// for easy data retrieval.
-request(url, { json: true }, (error, response, body) => {
-  // Printing the error if occurred
-//  if (error) console.log(error);
-  // Printing status code
-  // console.log(response.statusCode);
 
-  // Printing body
-//  console.log(body.results[0].characters);
-//  characters = body.results[0].characters
-  characters = body.results[0].characters.map(character => character.toString());
-//  console.log(characters)
-  userId = 18
-  const trial = `https://swapi-api.alx-tools.com/api/people/${userId}/`
-//  console.log(trial)
-  if (characters.includes(trial)) {
-//    console.log("here")
-  }  else {
-//  console.log("not here");
-}
-const trial2 = `https://swapi-api.alx-tools.com/api/people/${userId}/`
-request(trial2, { json: true }, (error, response, body) => {
-    console.log(body.films.length) 
-});
+const request = require('request');
+const url = process.argv[2];
+const characterId = '18';
+let count = 0;
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const data = JSON.parse(body);
+    data.results.forEach((film) => {
+      film.characters.forEach((character) => {
+        if (character.includes(characterId)) {
+          count += 1;
+        }
+      });
+    });
+    console.log(count);
+  }
 });
